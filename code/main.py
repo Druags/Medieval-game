7128377b370db630aa25d76a3eedc6055c60a429
Druags <anty.troll@mail.ru>
2023-08-04 18:40:09 +0300

    project initialization

---
+++ code/main.py
@@ -0,0 +1,25 @@
+import pygame, sys
+from settings import *
+
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        pygame.display.set_caption('My game')
+        self.clock = pygame.time.Clock()
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+            dt = self.clock.tick() / 1000
+
+            pygame.display.update()
+
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
