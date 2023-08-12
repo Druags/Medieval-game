c67eb23dea7426d4b2bbd39e73d1d4fc8f2d063a
Druags <anty.troll@mail.ru>
2023-08-12 21:28:40 +0300

    files for sprites

---
+++ data/Roof.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="Roof" tilewidth="32" tileheight="32" tilecount="558" columns="31">
+ <image source="tilesets/крыша.png" width="1000" height="604"/>
+</tileset>
